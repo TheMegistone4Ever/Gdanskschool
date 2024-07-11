77e9bd78dd21e7464f503826acd321160fdf88c0
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-07-11 22:11:08 +0300

    P-I curves for both modes are added.

--- main.py
+++ main.py
@@ -39,71 +39,62 @@ def plot_i_v_curves(data_files, i_v_mode):
     plt.show()
 
 
-# Define SOFC mode data files
-sofc_files = [
-    r".\mnt\data\800_SOFC_test_4_H2O.txt",
-    r".\mnt\data\800_SOFC_test_12_H2O.txt",
-    r".\mnt\data\800_SOFC_test_25_H2O.txt",
-    r".\mnt\data\800_SOFC_test_50_H2O.txt"
-]
-
-# Plot I-V curves for SOFC mode
-plot_i_v_curves(sofc_files, "SOFC")
-
-# Define SOEC mode data files
-soec_files = [
-    r".\mnt\data\800_SOEC_test_4_H2O.txt",
-    r".\mnt\data\800_SOEC_test_12_H2O.txt",
-    r".\mnt\data\800_SOEC_test_25_H2O.txt",
-    r".\mnt\data\800_SOEC_test_50_H2O.txt"
-]
-
-# Plot I-V curves for SOEC mode
-plot_i_v_curves(soec_files, "SOEC")
-
-# Plot P-I curves for SOFC mode
-plt.figure(figsize=(10, 6))
-for file in sofc_files:
-    data = pd.read_csv(file, sep=r"\s+")
-    power = data["E(Volts)"] * data["I(A/cm2)"]
-    plt.plot(data["I(A/cm2)"], power, label=f"{file.split('/')[-1].split('_')[3]} H2O")
-plt.xlabel("Current Density (A/cm2)")
-plt.ylabel("Power Density (W/cm2)")
-plt.title("P-I Curves for SOFC Mode")
-plt.legend(title="% H2O")
-plt.grid(True)
-plt.savefig(r".\plots\pi_curves_sofc_mode.png", dpi=150)
-plt.show()
-
-# Define impedance spectroscopy data files for both SOFC and SOEC modes
-impedance_files = {
-    "SOFC": [
-        r".\mnt\data\IS_800_SOFC_4_H2O.txt",
-        r".\mnt\data\IS_800_SOFC_12_H2O.txt",
-        r".\mnt\data\IS_800_SOFC_25_H2O.txt",
-        r".\mnt\data\IS_800_SOFC_50_H2O.txt"
-    ],
-    "SOEC": [
-        r".\mnt\data\IS_800_SOEC_4_H2O.txt",
-        r".\mnt\data\IS_800_SOEC_12_H2O.txt",
-        r".\mnt\data\IS_800_SOEC_25_H2O.txt",
-        r".\mnt\data\IS_800_SOEC_50_H2O.txt"
-    ]
-}
-
-# Plot Nyquist plots for both SOFC and SOEC modes
-for mode, files in impedance_files.items():
+def plot_p_i_curves(data_files, p_i_mode):
     plt.figure(figsize=(10, 6))
-    for file in files:
+    for f in data_files:
+        file_df = pd.read_csv(f, sep=r"\s+")
+        power = file_df["E(Volts)"] * file_df["I(A/cm2)"]
+        plt.plot(file_df["I(A/cm2)"], power, label=f"{f.split('/')[-1].split('_')[3]} H2O")
+    plt.xlabel("Current Density (A/cm2)")
+    plt.ylabel("Power Density (W/cm2)")
+    plt.title(f"P-I Curves for {p_i_mode} Mode")
+    plt.legend(title="% H2O")
+    plt.grid(True)
+    plt.savefig(rf".\plots\pi_curves_{p_i_mode.lower()}_mode.png", dpi=150)
+    plt.show()
+
+
+def plot_nyquist_plots(data_files, nyquist_mode):
+    plt.figure(figsize=(10, 6))
+    for file in data_files:
         data = pd.read_csv(file, sep=r"\s+")
         plt.plot(data["Z'"], data["Z''"], label=f"{file.split('/')[-1].split('_')[2]} H2O")
     plt.xlabel("Z' (Real Impedance, Ω)")
     plt.ylabel("Z'' (Imaginary Impedance, Ω)")
-    plt.title(f"Nyquist Plot for {mode} Mode")
+    plt.title(f"Nyquist Plot for {nyquist_mode} Mode")
     plt.legend(title="% H2O")
     plt.grid(True)
-    plt.savefig(rf".\plots\nyquist_plot_{mode.lower()}_mode.png", dpi=150)
+    plt.savefig(rf".\plots\nyquist_plot_{nyquist_mode.lower()}_mode.png", dpi=150)
     plt.show()
+
+
+# Define impedance spectroscopy data files for both SOFC and SOEC modes
+impedance_files = {
+    "SOFC": [
+        r".\mnt\data\800_SOFC_test_4_H2O.txt",
+        r".\mnt\data\800_SOFC_test_12_H2O.txt",
+        r".\mnt\data\800_SOFC_test_25_H2O.txt",
+        r".\mnt\data\800_SOFC_test_50_H2O.txt",
+    ],
+    "SOEC": [
+        r".\mnt\data\800_SOEC_test_4_H2O.txt",
+        r".\mnt\data\800_SOEC_test_12_H2O.txt",
+        r".\mnt\data\800_SOEC_test_25_H2O.txt",
+        r".\mnt\data\800_SOEC_test_50_H2O.txt",
+    ],
+}
+
+# Plot I-V curves for both SOFC and SOEC modes
+for mode, files in impedance_files.items():
+    plot_i_v_curves(files, mode)
+
+# Plot P-I curves for both SOFC and SOEC modes
+for mode, files in impedance_files.items():
+    plot_p_i_curves(files, mode)
+
+# Plot Nyquist plots for both SOFC and SOEC modes
+for mode, files in impedance_files.items():
+    plot_nyquist_plots(files, mode)
 
 # Load one impedance spectroscopy data file to check the column names
 data_example = pd.read_csv(r".\mnt\data\IS_800_SOFC_4_H2O.txt", sep=r"\s+")
